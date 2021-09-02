d8c98379bfd3523ef4c118f1ae29443d30804c13
Koupah <ktayl21@yahoo.com.au>
2021-09-03 00:33:48 +1000

    Fix Javadoc

--- src/main/java/io/github/thatkawaiisam/filare/BungeeConfiguration.java
+++ src/main/java/io/github/thatkawaiisam/filare/BungeeConfiguration.java
@@ -13,64 +13,63 @@ import java.io.OutputStream;
 
 public class BungeeConfiguration extends BaseConfiguration {
 
-    private Plugin plugin;
-    private Configuration base;
+	private Plugin plugin;
+	private Configuration base;
 
-    /**
-     * Bungee Configuration.
-     *
-     * @param plugin instance.
-     * @param name of file.
-     * @param directory of file.
-     */
-    public BungeeConfiguration(Plugin plugin, String name, String directory) {
-        super(name, directory);
-        this.plugin = plugin;
-    }
+	/**
+	 * Bungee Configuration.
+	 *
+	 * @param plugin    Plugin instance.
+	 * @param name      Name of file.
+	 * @param directory Directory of file.
+	 */
+	public BungeeConfiguration(Plugin plugin, String name, String directory) {
+		super(name, directory);
+		this.plugin = plugin;
+	}
 
-    @Override
-    void onCreate() {
-    	
-    	// Create any parent directories
-        plugin.getDataFolder().mkdir();
-        getFile().getParentFile().mkdirs();
+	@Override
+	void onCreate() {
 
-        // If file does not already exist, then grab it internally from the resources folder
-        if (!getFile().exists()) {
-            try {
-                getFile().createNewFile();
-                try (
-                        InputStream is = plugin.getResourceAsStream(getName() + ".yml");
-                        OutputStream os = new FileOutputStream(getFile())
-                ) {
-                    ByteStreams.copy(is, os);
-                }
-            } catch (IOException e) {
-                throw new RuntimeException("Unable to create configuration file", e);
-            }
+		// Create any parent directories
+		plugin.getDataFolder().mkdir();
+		getFile().getParentFile().mkdirs();
 
-        }
-    }
+		// If file does not already exist, then grab it internally from the resources
+		// folder
+		if (!getFile().exists()) {
+			try {
+				getFile().createNewFile();
+				try (InputStream is = plugin.getResourceAsStream(getName() + ".yml");
+						OutputStream os = new FileOutputStream(getFile())) {
+					ByteStreams.copy(is, os);
+				}
+			} catch (IOException e) {
+				throw new RuntimeException("Unable to create configuration file", e);
+			}
 
-    @Override
-    public void load() {
-        try {
-            base = ConfigurationProvider.getProvider(YamlConfiguration.class).load(getFile());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+		}
+	}
 
-    @Override
-    public void save() {
-        try {
-            ConfigurationProvider.getProvider(YamlConfiguration.class).save(base, getFile());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	@Override
+	public void load() {
+		try {
+			base = ConfigurationProvider.getProvider(YamlConfiguration.class).load(getFile());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public Configuration getImplementation() {
-        return base;
-    }
+	@Override
+	public void save() {
+		try {
+			ConfigurationProvider.getProvider(YamlConfiguration.class).save(base, getFile());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Configuration getImplementation() {
+		return base;
+	}
 }
